16664ee603243f660ad0909c3f9d27daa8dcb5ab
TianaSaurusWrecks <tianavioletwilson@gmail.com>
2025-10-25 20:42:30 +1100

    Question 2.1
    Question 2.1 created object myMovie with Title and Year

---
+++ script.js
@@ -0,0 +1,17 @@
+// Question 2.1 Build an object called "myMovie" 
+// with the following properties and data:
+//      Title: The Longest Day
+//      Year: 1964
+
+// Objects are like little containers holding pieces of related information
+
+// create object called myMovie
+const myMovie = { 
+    // "Title" is the key (property name) and "The Longest Day" is the value
+    Title: "The Longest Day", 
+    // "Year" is the key and "1964" is the value
+    Year: 1964
+
+};
+// Print object to console
+console.log(myMovie);
